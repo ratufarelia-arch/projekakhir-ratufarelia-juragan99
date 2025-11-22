d30e2fe81d88c1fa58130b0ee93dec632b69a44c
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-22 17:09:16 +0700

    update sidebar.blade.php

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -17,6 +17,14 @@
                 </flux:navlist.group>
             </flux:navlist>
 
+            @if(auth()->user()->isAdmin())
+                <flux:navlist variant="outline">
+                    <flux:navlist.group :heading="__('Administration')" class="grid">
+                        <flux:navlist.item icon="shield-check" :href="route('admin.dashboard')" :current="request()->routeIs('admin.dashboard')" wire:navigate>{{ __('Admin Dashboard') }}</flux:navlist.item>
+                    </flux:navlist.group>
+                </flux:navlist>
+            @endif
+
             <flux:spacer />
 
             <flux:navlist variant="outline">
