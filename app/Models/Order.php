890ade467c0a233e2b4140706c8e824cc1cdcabc
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-23 22:55:28 +0700

    update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
@@ -48,5 +49,10 @@ class Order extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function review(): HasOne
+    {
+        return $this->hasOne(Review::class);
     }
 }
