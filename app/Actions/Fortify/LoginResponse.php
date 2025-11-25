21ea4830e094579be10224670d24e3939e976cca
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-25 22:51:07 +0700

    feat: create LoginResponse.php

---
+++ app/Actions/Fortify/LoginResponse.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Actions\Fortify;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
+
+class LoginResponse implements LoginResponseContract
+{
+    /**
+     * Create an HTTP response that represents the object.
+     */
+    public function toResponse($request)
+    {
+        $redirect = $this->determineRedirectPath($request);
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect()->intended($redirect);
+    }
+
+    private function determineRedirectPath(Request $request): string
+    {
+        $user = $request->user();
+
+        if ($user?->hasRole(User::ROLE_ADMIN)) {
+            return route('dashboard');
+        }
+
+        return route('home');
+    }
+}
