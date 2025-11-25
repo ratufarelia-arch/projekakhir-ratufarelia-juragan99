718060e6ace8c3dc182da8dae3addf150142a7ff
Rifannvl <142275823+Rifannvl@users.noreply.github.com>
2025-11-26 01:46:10 +0700

    feat: create store.php

---
+++ config/store.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'timezone' => 'Asia/Jakarta',
+
+    'hours' => [
+        'monday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'tuesday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'wednesday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'thursday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'friday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'saturday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+        'sunday' => [
+            'open' => '08:00',
+            'close' => '22:00',
+        ],
+    ],
+];
